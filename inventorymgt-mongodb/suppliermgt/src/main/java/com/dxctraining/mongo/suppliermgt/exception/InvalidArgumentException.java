3339bda3d667e4438031af01250e197a6d473f53
amalpradeep-mv <amalpradeepmv@gmail.com>
2020-08-31 23:22:37 +0530

    sprint 17

--- inventorymgt-mongodb/suppliermgt/src/main/java/com/dxctraining/mongo/suppliermgt/exception/InvalidArgumentException.java
+++
@@ -1,8 +0,0 @@
-package com.dxctraining.mongo.suppliermgt.exception;
-
-public class InvalidArgumentException extends RuntimeException {
-
-	public InvalidArgumentException(String msg) {
-		super(msg);
-	}
-}
